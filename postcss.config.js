300cedbefb36510dd3a4beb195937c7afad994ae
acliunianc <1677158324@qq.com>
2024-11-28 10:07:37 +0800

    update：对库的样式使用hash，防止污染全局样式

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,8 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-modules': {
+      generateScopedName: '[name]__[local]___[hash:base64:5]',  // 使用哈希生成唯一类名
+    },
   },
 };
