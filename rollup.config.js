3cec0bf5ecab2d7e2c7dc77f385cfbb1e207bd61
acliunianc <1677158324@qq.com>
2025-11-07 15:04:55 +0800

    update：更新打包方案

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
+import postcss from "rollup-plugin-postcss";
+import clear from "rollup-plugin-clear";
 
 export default [
   {
@@ -13,6 +15,20 @@ export default [
     },
     external: ["react", "react-dom"],
     plugins: [
+      clear({
+        targets: ["dist"],
+      }),
+      postcss({
+        extract: true, // 将 CSS 提取到生成 JS 文件的同一位置，但扩展名 .css
+        minimize: true, // 是否压缩 CSS
+        inject: false, // 将 CSS 注入 <head>，当 extract：true 时它总是 false
+        sourceMap: true, // 是否生成 source map
+        modules: false, // 启用 CSS 模块或为 postcss-modules 设置选项
+        config: {
+          path: "./postcss.config.cjs", // 指定 postcss 配置文件的路径
+          ctx: {},
+        },
+      }),
       resolve(),
       commonjs(),
       typescript({
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
+import "./styles/index.css"
 export { default as FileManager } from "./components/FileManagerComp";
